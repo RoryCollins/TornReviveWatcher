fa3bf03ad8f3eb9724886a487b11dda6afaea8b6
Rory Collins <rory.collins@hotmail.co.uk>
2023-01-20 22:54:07 +0000

    now in CLI form

--- MemberList.py
+++ MemberList.py
@@ -11,6 +11,9 @@ class MemberList(object):
     def __init__(self, members):
         self.members = members
 
+    def filter_in(self, filterList):
+        return MemberList([(k, v) for k, v in self.members if k in filterList])
+
     def online_only(self):
         return MemberList([(k, v) for k, v in self.members if v['last_action']['status'] == 'Online'])
 
@@ -23,9 +26,17 @@ class MemberList(object):
     def revivable_only(self):
         return MemberList([(k, v) for k, v in self.members if is_revivable(k)])
 
+    def open_if_revivable(self):
+        for k, _ in self.members:
+            if is_revivable(k):
+                webbrowser.open(f'https://www.torn.com/profiles.php?XID={k}')
+            else:
+                print(f"not revivable: {k}")
+
     def plus(self, other):
         return MemberList(self.members + other.members)
 
-    def open_in_browser(self):
-        for k, v in self.members:
-            webbrowser.open(f'https://www.torn.com/profiles.php?XID={k}')
+    def print(self):
+        for k, _ in self.members:
+            print(k)
+
---
+++ revive.py
@@ -0,0 +1,33 @@
+import argparse
+import functools
+
+from MemberList import MemberList
+from TornApiService import *
+
+parser = argparse.ArgumentParser(description="Tool for revivers to find available targets. Comma separation for "
+                                             "multiple contracts, e.g. python3 ./revive.py --on 1234,4321 --off 5678")
+
+parser.add_argument("-n", "--on", help="Comma separated IDs for factions whose online members should be revived")
+parser.add_argument("-f", "--off", help="Comma separated IDs for factions whose offline members should be revived")
+
+args = parser.parse_args()
+
+onlineFactions = []
+offlineFactions = []
+
+
+def get_member_list(factionId):
+    return MemberList(get_faction_json(factionId)['members'].items())
+
+
+if args.on is not None:
+    for factionId in args.on.split(","):
+        onlineMemberList = get_member_list(factionId).online_only()
+        onlineFactions.append(onlineMemberList)
+
+if args.off is not None:
+    for faction in args.off.split(","):
+        offlineFactions.append(get_member_list(factionId).offline_only())
+
+targets = functools.reduce(lambda a, b: a.plus(b), onlineFactions + offlineFactions)
+targets.hospitalised_only().open_if_revivable()
